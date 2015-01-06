0b30918e325cec8c4ba6075de6d32db73269378f
Hongbo LU <gitawego@gmail.com>
2015-01-06 15:59:18 +0100

    format

--- src/SocketIO.js
+++ src/SocketIO.js
@@ -1,79 +1,80 @@
 //var io = require("socket.io-client/socket.io");
 import helper from './helper';
 
-    "use strict";
-    class SocketIO {
-        constructor(server,io) {
-            this.io = io;
-            if (server) {
-                this.connect(server,io);
-            }
+"use strict";
+class SocketIO {
+    constructor(server, io) {
+        this.io = io;
+        if (server) {
+            this.connect(server, io);
         }
-        connect(server,io) {
-            io = io || this.io;
-            if(!io){
-                return;
-            }
-            this.io = io;
-            this.socket = io(server);
-            this.socket.on('connect', () => {
-                this.connected = true;
-            });
-            this.socket.on('disconnect', () => {
-                console.log('disconnected');
-                this.connected = false;
-            });
-            this.socket.on('error', (evt)=> {
-                console.log('onerror', evt);
-            });
-        }
-
-        emit(evtName, msg, callback) {
-            if (callback) {
-                var uuid = helper.uuid(), rm;
-                msg._uuid = uuid;
-                this.once('disconnect', function () {
-                    rm && rm();
-                    callback({
-                        status: 1,
-                        error: new Error('disconnected')
-                    });
-                });
-                this.once('error', function (evt) {
-                    rm && rm();
-                    callback({
-                        status: 1,
-                        error: evt
-                    });
-                });
-                rm = this.on(evtName, function (resp) {
-                    if (resp._uuid === uuid) {
-                        console.log('socket resp', resp, uuid);
-                        rm();
-                        delete resp._uuid;
-                        callback(resp);
-                    }
-                });
-            }
-            console.log('evt', evtName, msg);
-            this.socket.emit(evtName, msg);
-            return this;
-        }
-
-        once(evtName, callback) {
-            var handler = this.on(evtName, function (data) {
-                handler();
-                callback(data);
-            });
-            return handler;
-        }
-
-        on(evtName, callback) {
-            this.socket.on(evtName, callback);
-            return ()=> {
-                this.socket.off(evtName, callback);
-            }
-        }
-
     }
-    export default SocketIO;
+
+    connect(server, io) {
+        io = io || this.io;
+        if (!io) {
+            return;
+        }
+        this.io = io;
+        this.socket = io(server);
+        this.socket.on('connect', () => {
+            this.connected = true;
+        });
+        this.socket.on('disconnect', () => {
+            console.log('disconnected');
+            this.connected = false;
+        });
+        this.socket.on('error', (evt)=> {
+            console.log('onerror', evt);
+        });
+    }
+
+    emit(evtName, msg, callback) {
+        if (callback) {
+            var uuid = helper.uuid(), rm;
+            msg._uuid = uuid;
+            this.once('disconnect', function () {
+                rm && rm();
+                callback({
+                    status: 1,
+                    error: new Error('disconnected')
+                });
+            });
+            this.once('error', function (evt) {
+                rm && rm();
+                callback({
+                    status: 1,
+                    error: evt
+                });
+            });
+            rm = this.on(evtName, function (resp) {
+                if (resp._uuid === uuid) {
+                    console.log('socket resp', resp, uuid);
+                    rm();
+                    delete resp._uuid;
+                    callback(resp);
+                }
+            });
+        }
+        console.log('evt', evtName, msg);
+        this.socket.emit(evtName, msg);
+        return this;
+    }
+
+    once(evtName, callback) {
+        var handler = this.on(evtName, function (data) {
+            handler();
+            callback(data);
+        });
+        return handler;
+    }
+
+    on(evtName, callback) {
+        this.socket.on(evtName, callback);
+        return ()=> {
+            this.socket.off(evtName, callback);
+        }
+    }
+
+}
+export default SocketIO;
--- src/binaryHelper.js
+++ src/binaryHelper.js
@@ -1,94 +1,94 @@
-    "use strict";
-    var binaryHelper = {
-        blobToArrayBuffer: function blobToArrayBuffer(blob) {
-            return new Promise(function(resolve,reject){
-                var reader = new FileReader();
-                reader.addEventListener("loadend", function () {
-                    // reader.result contains the contents of blob as a typed array
-                    resolve(this.result);
-                });
-                reader.addEventListener("error", reject);
-                reader.readAsArrayBuffer(blob);
+"use strict";
+var binaryHelper = {
+    blobToArrayBuffer: function blobToArrayBuffer(blob) {
+        return new Promise(function (resolve, reject) {
+            var reader = new FileReader();
+            reader.addEventListener("loadend", function () {
+                // reader.result contains the contents of blob as a typed array
+                resolve(this.result);
             });
-        },
-        arrayBufferToBinary: function ArrayBufferToString(buffer) {
-            var arr = new Uint8Array(buffer);
-            return binaryHelper.fromCharCodes(arr, buffer.byteLength);
-        },
-        arrayBufferToString: function (buffer) {
-            return binaryHelper.binaryToString(binaryHelper.arrayBufferToBinary(buffer));
-        },
-        /**
-         * for nodejs
-         * @param {ArrayBuffer} buffer
-         * @returns {Buffer}
-         */
-        arrayToBuffer: function (buffer) {
-            return new Buffer(new Uint8Array(buffer));
-        },
-        fromCharCodes: function (arr, len) {
-            if (typeof(len) !== 'number') {
-                len = arr.length;
-            }
-            var i = 0, str = "", fromCharCode = String.fromCharCode;
-            for (; i < len; i++) {
-                str += fromCharCode(arr[i]);
-            }
-            return str;
-        },
-        stringToArrayBuffer: function stringToArrayBuffer(string) {
-            return binaryHelper.stringToUint8Array(string).buffer;
-        },
-
-        binaryToString: function binaryToString(binary) {
-            var error;
-            try {
-                return decodeURIComponent(escape(binary));
-            } catch (_error) {
-                error = _error;
-                if (error instanceof URIError) {
-                    return binary;
-                } else {
-                    throw error;
-                }
-            }
-        },
-
-        stringToBinary: function stringToBinary(string) {
-            var chars, code, i, isUCS2, len, _i;
-
-            len = string.length;
-            chars = [];
-            isUCS2 = false;
-            for (i = _i = 0; 0 <= len ? _i < len : _i > len; i = 0 <= len ? ++_i : --_i) {
-                //code = String.prototype.charCodeAt.call(string, i);
-                code = string.charCodeAt(i);
-                if (code > 255) {
-                    isUCS2 = true;
-                    chars = null;
-                    break;
-                } else {
-                    chars.push(code);
-                }
-            }
-            if (isUCS2 === true) {
-                return unescape(encodeURIComponent(string));
-            } else {
-                return chars && binaryHelper.fromCharCodes(chars);
-            }
-        },
-
-        stringToUint8Array: function stringToUint8Array(string) {
-            var binary, binLen, buffer, chars, i, _i;
-            binary = binaryHelper.stringToBinary(string);
-            binLen = binary.length;
-            buffer = new ArrayBuffer(binLen);
-            chars = new Uint8Array(buffer);
-            for (i = _i = 0; 0 <= binLen ? _i < binLen : _i > binLen; i = 0 <= binLen ? ++_i : --_i) {
-                //chars[i] = String.prototype.charCodeAt.call(binary, i);
-                chars[i] = binary.charCodeAt(i);
-            }
-            return chars;
+            reader.addEventListener("error", reject);
+            reader.readAsArrayBuffer(blob);
+        });
+    },
+    arrayBufferToBinary: function ArrayBufferToString(buffer) {
+        var arr = new Uint8Array(buffer);
+        return binaryHelper.fromCharCodes(arr, buffer.byteLength);
+    },
+    arrayBufferToString: function (buffer) {
+        return binaryHelper.binaryToString(binaryHelper.arrayBufferToBinary(buffer));
+    },
+    /**
+     * for nodejs
+     * @param {ArrayBuffer} buffer
+     * @returns {Buffer}
+     */
+    arrayToBuffer: function (buffer) {
+        return new Buffer(new Uint8Array(buffer));
+    },
+    fromCharCodes: function (arr, len) {
+        if (typeof(len) !== 'number') {
+            len = arr.length;
         }
-    };
-    export default binaryHelper;
+        var i = 0, str = "", fromCharCode = String.fromCharCode;
+        for (; i < len; i++) {
+            str += fromCharCode(arr[i]);
+        }
+        return str;
+    },
+    stringToArrayBuffer: function stringToArrayBuffer(string) {
+        return binaryHelper.stringToUint8Array(string).buffer;
+    },
+
+    binaryToString: function binaryToString(binary) {
+        var error;
+        try {
+            return decodeURIComponent(escape(binary));
+        } catch (_error) {
+            error = _error;
+            if (error instanceof URIError) {
+                return binary;
+            } else {
+                throw error;
+            }
+        }
+    },
+
+    stringToBinary: function stringToBinary(string) {
+        var chars, code, i, isUCS2, len, _i;
+
+        len = string.length;
+        chars = [];
+        isUCS2 = false;
+        for (i = _i = 0; 0 <= len ? _i < len : _i > len; i = 0 <= len ? ++_i : --_i) {
+            //code = String.prototype.charCodeAt.call(string, i);
+            code = string.charCodeAt(i);
+            if (code > 255) {
+                isUCS2 = true;
+                chars = null;
+                break;
+            } else {
+                chars.push(code);
+            }
+        }
+        if (isUCS2 === true) {
+            return unescape(encodeURIComponent(string));
+        } else {
+            return chars && binaryHelper.fromCharCodes(chars);
+        }
+    },
+
+    stringToUint8Array: function stringToUint8Array(string) {
+        var binary, binLen, buffer, chars, i, _i;
+        binary = binaryHelper.stringToBinary(string);
+        binLen = binary.length;
+        buffer = new ArrayBuffer(binLen);
+        chars = new Uint8Array(buffer);
+        for (i = _i = 0; 0 <= binLen ? _i < binLen : _i > binLen; i = 0 <= binLen ? ++_i : --_i) {
+            //chars[i] = String.prototype.charCodeAt.call(binary, i);
+            chars[i] = binary.charCodeAt(i);
+        }
+        return chars;
+    }
+};
+export default binaryHelper;
