a10ed3df9afb51b63ded963e402c9c02a3e24000
Hongbo LU <gitawego@gmail.com>
2015-01-09 09:52:55 +0100

    updated

--- src/binaryHelper.js
+++ src/binaryHelper.js
@@ -89,6 +89,29 @@ var binaryHelper = {
             chars[i] = binary.charCodeAt(i);
         }
         return chars;
+    },
+    dataURLToBlob: function (dataURL) {
+        var BASE64_MARKER = ';base64,', parts, contentType, raw, rawLength, uInt8Array;
+        if (dataURL.indexOf(BASE64_MARKER) == -1) {
+            parts = dataURL.split(',');
+            contentType = parts[0].split(':')[1];
+            raw = decodeURIComponent(parts[1]);
+
+            return new Blob([raw], {type: contentType});
+        }
+
+        parts = dataURL.split(BASE64_MARKER);
+        contentType = parts[0].split(':')[1];
+        raw = window.atob(parts[1]);
+        rawLength = raw.length;
+
+        uInt8Array = new Uint8Array(rawLength);
+
+        for (var i = 0; i < rawLength; ++i) {
+            uInt8Array[i] = raw.charCodeAt(i);
+        }
+
+        return new Blob([uInt8Array], {type: contentType});
     }
 };
 export default binaryHelper;
--- src/helper.js
+++ src/helper.js
@@ -220,29 +220,6 @@ var helper = {
         context = canvas.getContext('2d');
         context.drawImage(src, 0, 0, canvasWidth, height);
         return canvas;
-    },
-    dataURLToBlob: function (dataURL) {
-        var BASE64_MARKER = ';base64,', parts, contentType, raw, rawLength, uInt8Array;
-        if (dataURL.indexOf(BASE64_MARKER) == -1) {
-            parts = dataURL.split(',');
-            contentType = parts[0].split(':')[1];
-            raw = decodeURIComponent(parts[1]);
-
-            return new Blob([raw], {type: contentType});
-        }
-
-        parts = dataURL.split(BASE64_MARKER);
-        contentType = parts[0].split(':')[1];
-        raw = window.atob(parts[1]);
-        rawLength = raw.length;
-
-        uInt8Array = new Uint8Array(rawLength);
-
-        for (var i = 0; i < rawLength; ++i) {
-            uInt8Array[i] = raw.charCodeAt(i);
-        }
-
-        return new Blob([uInt8Array], {type: contentType});
     }
 };
 helper.isNodeWebkit = (function () {
