e998c43e0edfa2c53291cf6d8e42b78404afb538
Hongbo LU <gitawego@gmail.com>
2015-02-09 09:34:15 +0100

    updated postinstall

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 SCRIPT_PATH=$(cd "$(dirname "$0")"; pwd);
 cd ${SCRIPT_PATH}
-npm install
-node_modules/.bin/6to5 src --out-dir dist --modules umd -r
+./node_modules/.bin/6to5 src --out-dir dist --modules umd -r
