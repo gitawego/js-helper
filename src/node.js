0b036144160a05f96f91b782d2816da80073c2c5
Hongbo LU <gitawego@gmail.com>
2015-11-02 22:19:42 +0100

    feat(openstack): added openstack wrapper for OVH

--- src/node.js
+++ src/node.js
@@ -1,28 +1,36 @@
+var Readable = require('stream').Readable;
 var node = {
-	getLocalIp:function(){
-		var ips = {};
-		var os = require('os');
-		var ifaces = os.networkInterfaces();
+  getLocalIp: function () {
+    var ips = {};
+    var os = require('os');
+    var ifaces = os.networkInterfaces();
 
-		Object.keys(ifaces).forEach(function (ifname) {
-		  var alias = 0;
+    Object.keys(ifaces).forEach(function (ifname) {
+      var alias = 0;
 
-		  ifaces[ifname].forEach(function (iface) {
-		    if ('IPv4' !== iface.family || iface.internal !== false) {
-		      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-		      return;
-		    }
+      ifaces[ifname].forEach(function (iface) {
+        if ('IPv4' !== iface.family || iface.internal !== false) {
+          // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+          return;
+        }
 
-		    if (alias >= 1) {
-		      // this single interface has multiple ipv4 addresses
-		      ips[ifname+':'+alias] = iface.address;
-		    } else {
-		      // this interface has only one ipv4 adress
-		      ips[ifname] = iface.address;
-		    }
-		  });
-		});
-		return ips;
-	}
+        if (alias >= 1) {
+          // this single interface has multiple ipv4 addresses
+          ips[ifname + ':' + alias] = iface.address;
+        } else {
+          // this interface has only one ipv4 adress
+          ips[ifname] = iface.address;
+        }
+      });
+    });
+    return ips;
+  },
+  streamize(str){
+    "use strict";
+    var s = new Readable;
+    s.push(str);    // the string you want
+    s.push(null);      // indicates end-of-file basically - the end of the stream
+    return s;
+  }
 };
 export default node;
