f303e577e4d3aadd8c1a0e363ca4fb4e7ae952a2
Hongbo LU <gitawego@gmail.com>
2016-04-22 23:38:40 +0200

    updated

--- src/topic.js
+++ src/topic.js
@@ -8,42 +8,42 @@ var evt = new CustomEvent();
  * @requires CustomEvent
  *
  */
- class Topic{
-    pub(topic){
-        //>>excludeStart("production", pragmas.production);
-        if (!this.hasTopic(topic)) {
-            console.warn('%c topic ' + topic + ' doesn\'t have subscriber yet','background:yellow');
-        }
-        //>>excludeEnd("production");
-        return evt.broadcast.apply(evt, arguments);
+class Topic {
+  pub(topic) {
+    // >>excludeStart("production", pragmas.production);
+    if (!this.hasTopic(topic)) {
+      console.warn('%c topic ' + topic + ' doesn\'t have subscriber yet', 'background:yellow');
     }
-    /**
-     * subscribe to a topic
-     * @method sub
-     * @param {String} topic
-     * @param {Function} fnc
-     * @param {Boolean} [once]
-     * @returns {{id:String,remove:Function}}
-     */
-    sub (topic, fnc, once) {
-        return evt[once ? 'once' : 'on'](topic, fnc);
-    }
-    /**
-     * unsubscribe a topic
-     * @method unsub
-     * @param {String} topic
-     * @param {Function|String} id
-     * @returns {*}
-     */
-    unsub (topic, id) {
-        return evt.off(topic, id);
-    }
-    hasTopic (topic) {
-        return evt.hasListeners(topic);
-    }
-    getTopics () {
-        return Object.keys(evt._listeners);
-    }
- }
+    // >>excludeEnd("production");
+    return evt.broadcast.apply(evt, arguments);
+  }
+  /**
+   * subscribe to a topic
+   * @method sub
+   * @param {String} topic
+   * @param {Function} fnc
+   * @param {Boolean} [once]
+   * @returns {{id:String,remove:Function}}
+   */
+  sub(topic, fnc, once) {
+    return evt[once ? 'once' : 'on'](topic, fnc);
+  }
+  /**
+   * unsubscribe a topic
+   * @method unsub
+   * @param {String} topic
+   * @param {Function|String} id
+   * @returns {*}
+   */
+  unsub(topic, id) {
+    return evt.off(topic, id);
+  }
+  hasTopic(topic) {
+    return evt.hasListeners(topic);
+  }
+  getTopics() {
+    return Object.keys(evt._listeners);
+  }
+}
 
 export default new Topic();
